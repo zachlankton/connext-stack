e721c11145118f193ac8eba0bb75b56ec30f0933
Zach Lankton <zachlankton@gmail.com>
2021-11-15 15:50:55 -0500

    started work on next-auth couchbase adapter

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,23 @@
-import { Ottoman, SearchConsistency } from "ottoman";
+import { getDefaultInstance, Ottoman, SearchConsistency } from "ottoman";
 
 console.log("setting up db...");
-const cnxStr = process.env.COUCHBASE_CONNECTION as string;
-const ottoman = new Ottoman({
-  consistency: SearchConsistency.LOCAL,
-});
+const ottoman =
+  getDefaultInstance() ||
+  new Ottoman({
+    consistency: SearchConsistency.LOCAL,
+  });
+
+export { ottoman };
 
 const db = {
   connect: async () => {
     console.log("connecting to db...");
-    return await ottoman.connect(cnxStr);
+    return await ottoman.connect({
+      connectionString: process.env.COUCHBASE_CONNECTION as string,
+      bucketName: process.env.COUCHBASE_BUCKET as string,
+      username: process.env.COUCHBASE_USER as string,
+      password: process.env.COUCHBASE_PW as string,
+    });
   },
 };
 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,23 @@
 import NextAuth, { Profile } from "next-auth";
 import { OAuthConfig } from "next-auth/providers";
 import Google from "next-auth/providers/google";
+import CouchbaseAdapter, {
+  adapterOptions,
+} from "../../../../next-auth-couch-adapter";
+
+const options: adapterOptions = {
+  connectionString: "couchbase://localhost",
+  bucketName: "connext",
+  username: "Administrator",
+  password: "1234567890",
+  ensureCollections: true,
+  ensureIndexes: true,
+  collectionNames: {
+    User: "TestUser",
+    Account: "TestAccount",
+    Session: "TestSession",
+  },
+};
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,19 +27,23 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET as string,
     }) as OAuthConfig<Profile>,
   ],
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      console.log("User: ", user);
-      console.log("Account ", account);
-      console.log("Profile: ", profile);
-      console.log("Email: ", email);
-      console.log("Creds: ", credentials);
-      return true;
-    },
-    async session({ session, user, token }) {
-      console.log("User:", user);
-      console.log("Token:", token);
-      return session;
-    },
+  jwt: {
+    secret: process.env.SECRET as string,
   },
+  adapter: CouchbaseAdapter(options),
+  // callbacks: {
+  //   async signIn({ user, account, profile, email, credentials }) {
+  //     console.log("User: ", user);
+  //     console.log("Account ", account);
+  //     console.log("Profile: ", profile);
+  //     console.log("Email: ", email);
+  //     console.log("Creds: ", credentials);
+  //     return true;
+  //   },
+  //   async session({ session, user, token }) {
+  //     console.log("User:", user);
+  //     console.log("Token:", token);
+  //     return session;
+  //   },
+  // },
 });
