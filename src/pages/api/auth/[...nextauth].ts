a840070f8263aa095d1586a84c50568e4747a5a7
Zach Lankton <zachlankton@gmail.com>
2021-11-20 19:09:56 +0000

    added an error check for next auth route

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,42 +2,50 @@ import NextAuth, { Profile } from "next-auth";
 import { OAuthConfig } from "next-auth/providers";
 import Google from "next-auth/providers/google";
 import CouchbaseAdapter, { adapterOptions } from "next-auth-couchbase-adapter";
+import { NextApiRequest, NextApiResponse } from "next";
+import { ottoman, connectionOptions } from "@/db/index";
 
 const options: adapterOptions = {
-  connectionString: "couchbase://localhost",
-  bucketName: "connext",
-  username: "Administrator",
-  password: "1234567890",
+  instance: ottoman,
+  ...connectionOptions,
   // ensure collectionS and indexes for quick setup in development (DON'T DO THIS IN PRODUCTION)
   ensureCollections: true,
   ensureIndexes: true,
 };
 
-export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }) as OAuthConfig<Profile>,
-  ],
-  jwt: {
-    secret: process.env.SECRET as string,
-  },
-  adapter: CouchbaseAdapter(options),
-  // callbacks: {
-  //   async signIn({ user, account, profile, email, credentials }) {
-  //     console.log("User: ", user);
-  //     console.log("Account ", account);
-  //     console.log("Profile: ", profile);
-  //     console.log("Email: ", email);
-  //     console.log("Creds: ", credentials);
-  //     return true;
-  //   },
-  //   async session({ session, user, token }) {
-  //     console.log("User:", user);
-  //     console.log("Token:", token);
-  //     return session;
-  //   },
-  // },
-});
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (!process.env.GOOGLE_ID || !process.env.GOOGLE_SECRET) {
+    throw new Error("Need to setup google oauth and add id and secret to .env.local (https://next-auth.js.org/providers/google)")
+  }
+
+  const nextAuthHandler = NextAuth({
+    // Configure one or more authentication providers
+    providers: [
+      Google({
+        clientId: process.env.GOOGLE_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET as string,
+      }) as OAuthConfig<Profile>,
+    ],
+    jwt: {
+      secret: process.env.SECRET as string,
+    },
+    adapter: CouchbaseAdapter(options),
+    // callbacks: {
+    //   async signIn({ user, account, profile, email, credentials }) {
+    //     console.log("User: ", user);
+    //     console.log("Account ", account);
+    //     console.log("Profile: ", profile);
+    //     console.log("Email: ", email);
+    //     console.log("Creds: ", credentials);
+    //     return true;
+    //   },
+    //   async session({ session, user, token }) {
+    //     console.log("User:", user);
+    //     console.log("Token:", token);
+    //     return session;
+    //   },
+    // },
+  });
+
+  await nextAuthHandler(req,res)
+}
