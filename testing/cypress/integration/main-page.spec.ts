fd23e8a4265352755d4266919254bc8d3fd15fdf
Zach Lankton <zachlankton@gmail.com>
2021-11-21 19:43:00 +0000

    cypress test skip auth when not configured

--- testing/cypress/integration/main-page.spec.ts
+++ testing/cypress/integration/main-page.spec.ts
@@ -1,4 +1,9 @@
+
 describe("Page Tests", () => {
+  const username = Cypress.env("CYPRESS_GOOGLE_USER");
+  const password = Cypress.env("CYPRESS_GOOGLE_PW");
+  const loginUrl = Cypress.env("CYPRESS_SITE_NAME");
+  const cookieName = Cypress.env("CYPRESS_COOKIE_NAME");
   it("Index Page Loads", () => {
     cy.visit("http://localhost:3000");
     cy.request("http://localhost:3000/").its("status").should("equal", 200);
@@ -11,70 +16,71 @@ describe("Page Tests", () => {
       .should("equal", 200);
   });
 
-  it("Next Auth Sign In", () => {
-    const username = Cypress.env("CYPRESS_GOOGLE_USER");
-    const password = Cypress.env("CYPRESS_GOOGLE_PW");
-    const loginUrl = Cypress.env("CYPRESS_SITE_NAME");
-    const cookieName = Cypress.env("CYPRESS_COOKIE_NAME");
-    const socialLoginOptions = {
-      username,
-      password,
-      loginUrl,
-      headless: true,
-      logs: false,
-      isPopup: false,
-      loginSelector: ".sign-in",
-      postLoginSelector: ".signed-in-as",
-    };
+  if (username !== undefined) {
+    it("Next Auth Sign In", () => {
+        
+      const socialLoginOptions = {
+        username,
+        password,
+        loginUrl,
+        headless: true,
+        logs: false,
+        isPopup: false,
+        loginSelector: ".sign-in",
+        postLoginSelector: ".signed-in-as",
+      };
 
-    cy.task("GoogleSocialLogin", socialLoginOptions).then(({ cookies }) => {
-      cy.clearCookies();
-      const cookie = cookies
-        .filter((cookie) => cookie.name === cookieName)
-        .pop();
-      if (cookie) {
-        cy.setCookie("next-auth.session-token", cookie.value, {
-          domain: cookie.domain,
-          expiry: cookie.expires,
-          httpOnly: cookie.httpOnly,
-          path: cookie.path,
-          secure: cookie.secure,
-        });
+      cy.task("GoogleSocialLogin", socialLoginOptions).then(({ cookies }) => {
+        cy.clearCookies();
+        const cookie = cookies
+          .filter((cookie) => cookie.name === cookieName)
+          .pop();
+        if (cookie) {
+          cy.setCookie("next-auth.session-token", cookie.value, {
+            domain: cookie.domain,
+            expiry: cookie.expires,
+            httpOnly: cookie.httpOnly,
+            path: cookie.path,
+            secure: cookie.secure,
+          });
 
-        Cypress.Cookies.defaults({
-          preserve: "next-auth.session-token",
-        });
-      }
+          Cypress.Cookies.defaults({
+            preserve: "next-auth.session-token",
+          });
+        }
+      });
     });
-  });
 
-  it("User Profile Navigation Test", () => {
-    cy.intercept("/api/auth/session").as("getSession");
-    cy.visit("http://localhost:3000");
-    cy.get(".signed-in-as").click();
-    cy.get(".user-profile-title").should("have.text", "User Profile");
-    cy.get("header>section.mid").click();
-    cy.url().should("include", "/");
-  });
+    it("User Profile Navigation Test", () => {
+    
+      cy.intercept("/api/auth/session").as("getSession");
+      cy.visit("http://localhost:3000");
+      cy.get(".signed-in-as").click();
+      cy.get(".user-profile-title").should("have.text", "User Profile");
+      cy.get("header>section.mid").click();
+      cy.url().should("include", "/");
+    });
 
-  it("Signed In and Sign out", () => {
-    cy.intercept("/api/auth/session").as("getSession");
-    cy.intercept("/api/auth/providers", (req) => {
-      req.redirect("/");
-    }).as("signInGoogle");
-    cy.visit("http://localhost:3000");
-    cy.wait("@getSession");
-    cy.get(".signed-in-as")
-      .should("have.attr", "alt")
-      .and("include", "gmail.com");
+    it("Signed In and Sign out", () => {
+    
+      cy.intercept("/api/auth/session").as("getSession");
+      cy.intercept("/api/auth/providers", (req) => {
+        req.redirect("/");
+      }).as("signInGoogle");
+      cy.visit("http://localhost:3000");
+      cy.wait("@getSession");
+      cy.get(".signed-in-as")
+        .should("have.attr", "alt")
+        .and("include", "gmail.com");
 
-    cy.get(".sign-out").click();
-    cy.wait("@getSession");
-    cy.get(".sign-in").click();
-    cy.wait("@signInGoogle");
-    cy.url().should("include", "/");
-    cy.clearCookies();
-  });
+      cy.get(".sign-out").click();
+      cy.wait("@getSession");
+      cy.get(".sign-in").click();
+      cy.wait("@signInGoogle");
+      cy.url().should("include", "/");
+      cy.clearCookies();
+    });
+  }
 
   it("404 Test", () => {
     cy.visit("http://localhost:3000/pageDoesNotExits", {
