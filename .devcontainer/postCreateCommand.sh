563cd6e4bb1ca8f341d848100fbda16acda10780
Zach Lankton <zachlankton@gmail.com>
2021-11-21 19:42:17 +0000

    added cypress dependencies to devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -15,6 +15,8 @@ exit_check() {
 
 git clone https://github.com/zachlankton/connext-stack.git . 
 set -e
+apt update -y
+apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 npm install 
 cp .env.example .env.local
 echo "SECRET=$(openssl rand -hex 32)" >> .env.local 
